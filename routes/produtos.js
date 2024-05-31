abb046196e22094435fda3c90566caf4197e7987
Alisson Fernandes Bernardo <al060176@intelbras.com.br>
2024-05-31 16:06:05 -0300

    envio rest api teste

---
+++ routes/produtos.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+
+// RETORNA TODOS OS PRODUTOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: "usando o GET dentro da rota de produtos"
+    });
+});
+
+// INSERE UM PRODUTO
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Insere um produto'
+    })
+});
+
+// RETORNA OS DADOS DE UM PRODUTO
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto
+
+    if (id === 'especial') {
+        res.status(200).send({
+            mensagem: 'Você encontrou o ID especial',
+            id: id
+        })
+    }else{
+        res.status(200).send({
+            mensagem: 'Você passou um ID'
+        });
+    }
+})     
+
+// ALTERA UM PRODUTO
+router.patch('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: "Produto alterado"
+    });
+});
+
+// EXCLUI UM PRODUTO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Produto excluido"
+    });
+});
+
+module.exports = router; 
